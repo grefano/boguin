59a067752d1e6e0097ec8f22d9e0f69becf624a7
grefano <gabrielnieed@gmail.com>
2025-08-23 21:27:45 -0300

    better (hopefully) loading state on channel page

--- src/Channel.tsx
+++ src/Channel.tsx
@@ -3,44 +3,76 @@ import Video from "./components/Video"
 import { useParams } from "react-router-dom"
 import { get_cloud_url_thumbnail } from './util/cloudinaryUrls';
 
+type StateLoad = 'loading' | 'success' | 'notfound' | 'error'
+
 function Channel(){
     const [videos, setVideos] = useState<any[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [stateLoadUser, setStateLoadUser] = useState<StateLoad>('loading')
+    const [stateLoadVideos, setStateLoadVideos] = useState<StateLoad>('loading')
 
     const params = useParams()
     const channelId = params.id
 
     useEffect(() => {
+        const fetchChannel = async () => {
+            try {
+                const response = await fetch(import.meta.env.VITE_URL_SERVER + `/users/${channelId}`, {
+                    headers: {'ngrok-skip-browser-warning': 'true'}
+                })
+                console.log(response)
+                setStateLoadUser(response.ok ? 'success' : 'notfound')
+            } catch (error){
+                setStateLoadUser('error')
+                console.log(error)
+            }
+        }
+        fetchChannel()
+    }, [])
+    useEffect(() => {
         const fetchVideos = async () => {
             try {
                 const response = await fetch(import.meta.env.VITE_URL_SERVER + `/videos/users/${channelId}`, {
-                    headers: {
-                        'ngrok-skip-browser-warning': 'true'
-                    }
+                    headers: { 'ngrok-skip-browser-warning': 'true' }
                 })
                 const data = await response.json()
                 console.log(data)
                 setVideos(data)
-                setIsLoading(false)
+                setStateLoadVideos('success')
             } catch (error){
-                setIsLoading(true)
+                setStateLoadVideos('error')
                 console.error(error)
             }
         }
         fetchVideos()
     }, [])
-    if (isLoading){
-        return (<div>loading</div>)
-    }
     return (<>
-    <h1>{channelId}</h1>
-        <div id="ctn-channel-videos">
-            {
-                videos.map(video => (
-                    <Video title={video.title} thumbnail={get_cloud_url_thumbnail(video.id_thumb)} video={video.id}/>
-                ))
-            }
-        </div>
+        {stateLoadUser == 'loading'?
+            (<div>carregando</div>)
+        :null}
+        {stateLoadUser == 'notfound' || stateLoadUser == 'error'?
+            (<div>usuário não encontrado</div>)
+        :null}
+        {stateLoadUser == 'success'?
+            <>
+                <h1>{channelId}</h1>
+                <div id="ctn-channel-videos">
+                {stateLoadVideos == 'loading'?
+                    (<div>carregando</div>)
+                :null}
+                {stateLoadVideos == 'notfound' || stateLoadVideos == 'error'?
+                    (<div>vídeos não encontrados</div>)
+                :null}
+                {stateLoadVideos == 'success'?
+                    videos.map(video => (
+                        <Video title={video.title} thumbnail={get_cloud_url_thumbnail(video.id_thumb)} video={video.id}/>
+                    ))
+                :null}
+                </div>
+            </>
+        :null}
+            
+        
     </>)
 }
 
