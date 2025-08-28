5fb94145ddd0d0cba298437c417570d250d5dfd3
grefano <gabrielnieed@gmail.com>
2025-08-28 01:27:29 -0300

    removing unused variables

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { useDebugValue, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 interface PropsNormal {
     demo?: false,
