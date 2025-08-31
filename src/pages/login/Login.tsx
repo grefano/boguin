a667afd35fcdc0a62c1620a848e2987788d2ce98
grefano <gabrielnieed@gmail.com>
2025-08-30 23:37:04 -0300

    creating account leads to home page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -12,10 +12,78 @@ function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [inputsErrorMsg, setInputsErrorMsg] = useState<TypeInputsErrorMsg>({username: null, password: null})
-
+    const [accountMsg, setAccountMsg] = useState<string | null>(null)
+    
     const { login, isAuthenticated } = useAuth()
     const navigate = useNavigate()
+    
+    const seterrormsg = (obj: Object) => {
+        setInputsErrorMsg(prevState => ({
+            ...prevState,
+            ...obj
+        }))
+    }
+    const handleRegister = async () => {
+        const response = await fetch(import.meta.env.VITE_URL_SERVER + '/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        console.log(response)
+        if (response.ok){
+            console.log('set msg')
+            setAccountMsg('Conta criada com sucesso')
+            const data = await response.json()
+            login(data.token, username)
+        } else {
+            // const data = await response.json()
+            // setAccountMsg(`Conta não foi criada, ${data}`)
+            switch (response.status){
+                case 409:
+                    seterrormsg({username: 'Já existe uma conta com esse nome de usuário'})
+                    break;
+                default:
+                    console.error('Registration failed', (await response.json()).error)
+            }
+    
+        }
 
+    }
+    const handleLogin = async () => {
+        const response = await fetch(import.meta.env.VITE_URL_SERVER + '/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        if (response.ok) {
+            console.log('login sucessful')
+            const data = await response.json()
+            console.log(data)
+            login(data.token, username)
+            console.log('vorta pra dashbord')
+            
+            
+        } else {
+            switch (response.status){
+                case 401:
+                    seterrormsg({password: (await response.json()).error})
+                    break;
+                default:
+                    console.error('Login failed', (await response.json()).error)
+            }
+    
+        }
+    }
     useEffect(() => {
         console.log('useeffect')
         if (isAuthenticated){
@@ -50,61 +118,21 @@ function Login() {
         setUsername(processInputUsername(e.target.value))
     }
 
-    const handleSubmit = async (event: React.FormEvent, endpoint: 'login' | 'register') =>{
+    const handleSubmit = async (event: React.FormEvent, fnFetch: () => void) =>{
         event.preventDefault()
         if (!username || !password){
             return;
         }
 
         console.log('vai enviar')
-
-        const response = await fetch(import.meta.env.VITE_URL_SERVER + '/auth/' + endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        })
-        console.log(await response)
-        if (response.ok) {
-            console.log('login sucessful')
-            const data = await response.json()
-            console.log(data)
-            login(data.token, username)
-            console.log('vorta pra dashbord')
-            
-            
-        } else {
-            console.log(response.status)
-            if (response.status == 409){
-                // ja existe um usuario com esse nome
-                const data = await response.json()
-                console.log(data)
-                setInputsErrorMsg(({
-                    ...inputsErrorMsg,
-                    username: data.error
-                }))
-            } else if (response.status == 401) {
-                const data = await response.json()
-                console.log(data)
-                setInputsErrorMsg(({
-                    ...inputsErrorMsg,
-                    password: data.error
-                }))
-            } else {
-                const data = await response.json()
-                console.error('Login failed:', data.error   )
-            }
-
-        }
+        fnFetch()
+        
     }
     return (
         <div id="ctn-login">
         <h1 className='text-gg'>Login</h1>
         <form id="form-login">
+            {accountMsg ? (<div className='form-feedback'>{accountMsg}</div>) : null}
             <div>
                 <label htmlFor="fuser" className='text-m'>username:</label>
                 <input pattern="[a-z]+" className='text-p' type="text" name="" value={username} id="fuser" placeholder="unique username" onChange={handleUsername}/>
@@ -117,8 +145,8 @@ function Login() {
             </div>
             <div className='form-error'>{inputsErrorMsg.password}</div>
             <div id="ctn-login-buttons">
-                <button onClick={(e) => handleSubmit(e, 'login')}>Entrar</button>
-                <button onClick={(e) => handleSubmit(e, 'register')}>Criar conta</button>
+                <button onClick={(e) => handleSubmit(e, handleLogin)}>Entrar</button>
+                <button onClick={(e) => handleSubmit(e, handleRegister)}>Criar conta</button>
             </div>
         </form>
 
